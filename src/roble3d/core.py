845aaee733dcb2d2368ab9f3009c266503149804
sugayu <sugayu@aoni.waseda.jp>
2025-10-07 15:41:24 +0900

    First commit

---
+++ src/roble3d/core.py
@@ -0,0 +1,20 @@
+'''Core items of roble3d.'''
+
+from __future__ import annotations
+from logging import getLogger
+import numpy as np
+
+__all__ = ['Barrel']
+
+logger = getLogger(__name__)
+
+
+##
+class Barrel:
+    '''Entry point of roble3d.'''
+
+    def __init__(self) -> None:
+        pass
+
+    def extract1d(self) -> None:
+        pass
